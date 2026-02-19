a4e2d9e61e5431caea96ed356da132845718cf14
Hope1133 <soroko03@list.ru>
2026-02-19 18:49:37 +0300

    antifraud_features

--- Antifraud-text-scoring/src/feautures.py
+++ Antifraud-text-scoring/src/feautures.py
@@ -1,89 +1,47 @@
 import re
 import pandas as pd
-from collections import Counter
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
+
+URL_PATTERN = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+])+'
+PHONE_PATTERN = r"\+?\d[\d\-\s]{7,}\d"
+MONEY_PATTERN = r'\$|€|£|usd|eur'
+
+FRAUD_KEYWORDS = [
+    "win", "prize", "urgent", "verify",
+    "bank", "card", "password", "blocked", "confirm"
+]
 
 
 def clean_text(text: str) -> str:
-    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+])+'
-    phone_pattern = r"\+?\d[\d\-\s]{7,}\d"
+    if not isinstance(text, str):
+        return ""
 
     text = text.lower()
-    text = re.sub(url_pattern, " URL ", text)
-    text = re.sub(phone_pattern, " PHONE ", text)
+    text = re.sub(URL_PATTERN, " URL ", text)
+    text = re.sub(PHONE_PATTERN, " PHONE ", text)
     text = re.sub(r"\d+", " NUM ", text)
     text = re.sub(r"[^\w\s]", " ", text)
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
+def extract_numeric_features(X: pd.DataFrame) -> pd.DataFrame:
+    Xc = X.copy()
+    text = Xc["message"].fillna("")
 
-def extract_numeric_features(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
+    features = pd.DataFrame(index=Xc.index)
 
-    df['text_len'] = df['message'].str.len()
-    df['word_num'] = df['message'].str.split().apply(lambda x: len(x))
-    df['dig_num'] = df['message'].str.count(r"\d")
-    df['caps_ratio'] = df['message'].apply(lambda x : sum([i.isupper() for i in x]) / len(x))
-    df['num_special'] = df['message'].str.count(r"[^\w\s]")
-    df['num_words'] = df['message'].str.split().str.len()
-    df['num_money'] = df['message'].str.count(r'\$|€|£|usd|eur').astype(int)
+    features["text_len"] = text.str.len()
+    features["num_words"] = text.str.split().str.len()
+    features["digit_count"] = text.str.count(r"\d")
 
-    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+])+'
-    df['url_count'] = df['message'].apply(lambda x : len(re.findall(url_pattern, x)))
-    # df['urls'] = df['message'].apply(lambda x : re.findall(url_pattern, x))
+    features["caps_ratio"] = text.apply(lambda x: sum(c.isupper() for c in x) / len(x) if len(x) > 0 else 0)
 
-    phone_pattern = r"\+?\d[\d\-\s]{7,}\d"
-    df['phone_count'] = df['message'].apply(lambda x : len(re.findall(phone_pattern, x)))
+    features["special_count"] = text.str.count(r"[^\w\s]")
+    features["money_count"] = text.str.count(MONEY_PATTERN)
+    features["url_count"] = text.str.count(URL_PATTERN)
+    features["phone_count"] = text.str.count(PHONE_PATTERN)
 
-    return df[['text_len', 'word_num', 'dig_num', 'caps_ratio', 'num_special', 'num_words', 'num_money', 'url_count', 'phone_count']]
+    features["spam_words_count"] = text.apply(
+        lambda x: sum(word in FRAUD_KEYWORDS for word in x.split())
+    )
 
-
-def count_spam_words(text: str, fraud_keywords: list) -> int:
-    c = 0
-    for word in text.split():
-        if word in fraud_keywords:
-            c+=1
-    return(c)
-
-def spam_words_feature(df: pd.DataFrame) -> pd.DataFrame:
-    df['message'] = df['message'].apply(clean_text)
-    spam_messanges = df.loc[df['label'].eq(1), 'message'].apply(lambda x : re.findall('[a-zA-Z]+', x))
-    spam_words_count = Counter()
-    for m in spam_messanges:
-        spam_words_count.update(m)
-    spam_words = set([el for el, count in spam_words_count.most_common(50)])
-
-    non_spam_messanges = df.loc[df['label'].eq(0), 'message'].apply(lambda x : re.findall('[a-zA-Z]+', x))
-    non_spam_words_count = Counter()
-    for m in non_spam_messanges:
-        non_spam_words_count.update(m)
-    non_spam_words = set([el for el, count in non_spam_words_count.most_common(100)])
-
-    fraud_keywords = spam_words - non_spam_words
-
-    df['spam_words_count'] = df['message'].apply(count_spam_words(fraud_keywords))
-
-    return df['spam_words_count']
-
-def NLP_vectorization(df: pd.DataFrame) -> pd.DataFrame:
-    df['message'] = df['message'].apply(clean_text)
-    tfidf = TfidfVectorizer(max_features=2000, ngram_range=(1,2), stop_words='english', min_df=2)
-    X_tfidf = tfidf.fit_transform(df['message'])
-
-    # Cнижаем размерность до 20 компонент
-    svd = TruncatedSVD(n_components=50, random_state=42)
-    X_tfidf_reduced = svd.fit_transform(X_tfidf)
-
-    # Создаём DataFrame с компонентами
-    tfidf_cols = [f'tfidf_svd_{i}' for i in range(X_tfidf_reduced.shape[1])]
-    df_tfidf = pd.DataFrame(X_tfidf_reduced, columns=tfidf_cols)
-
-    return df_tfidf
-
-def feauture_engineering(df: pd.DataFrame) -> pd.DataFrame:
-    numeric_features = extract_numeric_features(df)
-    fraud_features = spam_words_feature(df)
-    nlp_features = NLP_vectorization(df)
-    df_for_train = pd.concat([numeric_features, fraud_features, nlp_features], axis=1)
-    df_for_train = df_for_train.dropna().reset_index(drop=True) 
+    return features
--- Antifraud-text-scoring/src/train.py
+++ Antifraud-text-scoring/src/train.py
@@ -5,9 +5,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 
-from features import clean_text, extract_numeric_features
+from features import feauture_engineering
 
 
 def load_data(path):
@@ -16,6 +17,20 @@ def load_data(path):
     df["label"] = df["label"].map({"ham": 0, "spam": 1})
     return df.dropna()
 
+def NLP_vectorization(df: pd.DataFrame) -> pd.DataFrame:
+    clean = df['message'].apply(clean_text)
+    tfidf = TfidfVectorizer(max_features=2000, ngram_range=(1,2), stop_words='english', min_df=2)
+    X_tfidf = tfidf.fit_transform(clean)
+
+    # Cнижаем размерность до 20 компонент
+    svd = TruncatedSVD(n_components=50, random_state=42)
+    X_tfidf_reduced = svd.fit_transform(X_tfidf)
+
+    # Создаём DataFrame с компонентами
+    tfidf_cols = [f'tfidf_svd_{i}' for i in range(X_tfidf_reduced.shape[1])]
+    df_tfidf = pd.DataFrame(X_tfidf_reduced, columns=tfidf_cols)
+
+    return df_tfidf
 
 def build_pipeline():
     text_transformer = TfidfVectorizer(
@@ -47,18 +62,13 @@ def build_pipeline():
 
 
 def main():
-    df = load_data("data/raw.csv")
+    df = load_data("data/data.csv")
+    df['label'] = df['label'].replace({'ham' : 0, 'spam' : 1})
 
-    df["message"] = df["message"].apply(clean_text)
-    numeric_features = extract_numeric_features(df)
-    df = pd.concat([df, numeric_features], axis=1)
-
-    X = df.drop("label", axis=1)
+    X = feauture_engineering(df.drop("label", axis=1))
     y = df["label"]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, stratify=y, random_state=42
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
     pipeline = build_pipeline()
     pipeline.fit(X_train, y_train)
@@ -68,3 +78,74 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+import re
+import pandas as pd
+from collections import Counter
+
+url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+])+'
+phone_pattern = r"\+?\d[\d\-\s]{7,}\d"
+
+def clean_text(text: str) -> str:
+    text = text.lower()
+    text = re.sub(url_pattern, " URL ", text)
+    text = re.sub(phone_pattern, " PHONE ", text)
+    text = re.sub(r"\d+", " NUM ", text)
+    text = re.sub(r"[^\w\s]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
+
+
+def extract_numeric_features(X: pd.DataFrame) -> pd.DataFrame:
+ Xc = X.copy()
+
+ Xc['text_len'] = X['message'].str.len()
+ Xc['num_words'] = X['message'].str.split().str.len()
+ Xc['dig_num'] = X['message'].str.count(r"\d")
+ Xc['caps_ratio'] = X['message'].apply(lambda x : sum([i.isupper() for i in x]) / len(x))
+ Xc['num_special'] = X['message'].str.count(r"[^\w\s]")
+ Xc['money_count'] = X['message'].str.count(r'\$|€|£|usd|eur').astype(int)
+ Xc['url_count'] = X['message'].apply(lambda x : len(re.findall(url_pattern, x)))
+ Xc['phone_count'] = X['message'].apply(lambda x : len(re.findall(phone_pattern, x)))
+
+ return Xc[['text_len', 'num_words', 'dig_num', 'caps_ratio', 'num_special', 'money_count', 'url_count', 'phone_count']]
+
+
+def count_spam_words(text: str, fraud_keywords: list) -> int:
+    c = 0
+    for word in text.split():
+        if word in fraud_keywords:
+            c+=1
+    return(c)
+
+def search_fraud_keywords(df: pd.DataFrame) -> list: 
+    df['message'] = df['message'].apply(clean_text)
+    spam_messanges = df.loc[df['label'].eq(1), 'message'].apply(lambda x : re.findall('[a-zA-Z]+', x))
+    spam_words_count = Counter()
+    for m in spam_messanges:
+        spam_words_count.update(m)
+    spam_words = set([el for el, count in spam_words_count.most_common(50)])
+
+    non_spam_messanges = df.loc[df['label'].eq(0), 'message'].apply(lambda x : re.findall('[a-zA-Z]+', x))
+    non_spam_words_count = Counter()
+    for m in non_spam_messanges:
+        non_spam_words_count.update(m)
+    non_spam_words = set([el for el, count in non_spam_words_count.most_common(100)])
+
+    fraud_keywords = spam_words - non_spam_words
+    return fraud_keywords
+
+def spam_words_feature(X: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
+    X['spam_words_count'] = X['message'].apply(count_spam_words(search_fraud_keywords(df)))
+    return X['spam_words_count']
+
+def feature_engineering(X: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
+    X_for_train = extract_numeric_features(X)
+    fraud_keywords = search_fraud_keywords(df)
+    X_for_train['spam_words_count'] = X['message'].apply(count_spam_words(fraud_keywords))
+    X_for_train = X_for_train.dropna().reset_index(drop=True) 
+
+    return X_for_train
